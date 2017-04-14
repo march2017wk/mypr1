b94018a5667cbf4e14d091b43cd2b12f3a43a4e1
venkatgn <venkatadrin.g@gmail.com>
2017-04-14 20:02:13 +0530

    updated sample prg with new f2 method

--- Mymvnproj/src/test/java/yahoo/Sample.java
+++ Mymvnproj/src/test/java/yahoo/Sample.java
@@ -5,5 +5,10 @@ public class Sample
  public void f1()
  {
 	 System.out.println("hello");
+	 System.out.println("dfkgdfg");
+ }
+ public void f2()
+ {
+	 System.out.println("added new f2 method");
  }
 }
